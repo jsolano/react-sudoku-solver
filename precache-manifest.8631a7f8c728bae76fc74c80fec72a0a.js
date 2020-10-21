36d95731fec29b7157cee0e87d8af4b89af989e0
J.P. Solano <juanp.solano@gmail.com>
2020-10-21 11:55:54 -0400

    Updates

---
+++ precache-manifest.8631a7f8c728bae76fc74c80fec72a0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "056e8c239b916f5cb413",
+    "url": "/react-sudoku-solver/static/css/main.4728cc33.chunk.css"
+  },
+  {
+    "revision": "056e8c239b916f5cb413",
+    "url": "/react-sudoku-solver/static/js/main.056e8c23.chunk.js"
+  },
+  {
+    "revision": "da47719a81db4fd694ab",
+    "url": "/react-sudoku-solver/static/js/1.da47719a.chunk.js"
+  },
+  {
+    "revision": "10f464cc883d2d6afef8",
+    "url": "/react-sudoku-solver/static/js/runtime~main.10f464cc.js"
+  },
+  {
+    "revision": "b093b40dfd25639f70e3e5d4bbf74683",
+    "url": "/react-sudoku-solver/static/media/nextstack-logo.b093b40d.png"
+  },
+  {
+    "revision": "900a09897449206755c3c338c9cd9de0",
+    "url": "/react-sudoku-solver/static/media/jp.900a0989.png"
+  },
+  {
+    "revision": "9b25be1882eaf7c5b7be7acff1fc5137",
+    "url": "/react-sudoku-solver/index.html"
+  }
+];
